ab0e55ba8fce07800f670bb162f7d1cba9669fe3
Nunna <Nunna@host.docker.internal>
2022-01-12 16:58:38 +0100

    java commit

---
+++ TestProject/src/nunna/java/testjava.java
@@ -0,0 +1,12 @@
+package nunna.java;
+
+public class testjava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("You entered");
+
+	}
+
+}
